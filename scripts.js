6ae25f71fa5a7a7e71a7222ac5deb291f7a5234b
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-10-07 12:04:38 +0200

    done

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 console.log(document.title);
+
+//document.getElementById("gen-1").textContent = "Generasión 1 Pokimon";
+//o
+const gen1 = document.getElementById("gen-1");
+gen1.textContent = "Generasión 1 Pokimon";
+gen1.style.backgroundColor = "red";
+
+
+console.log(document.domain)
+console.log(document.URL);
+console.log(document.querySelectorAll("img"));
+const link = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+
+//document.querySelectorAll("img").setAttribute("src", link);
+
+
+const images = document.querySelectorAll("img");
+for (let i = 0; i < images.length;  i++ ){
+images[i].setAttribute("src", link);
+}
+
+//Cambia el fondo de todos los infocard-lg-data text-muted para todos los Pokimon voladores itype flying
+const imagesVoladoras = document.querySelectorAll(".infocard .infocard-lg-data small a.itype.flying");
+for (let i = 0; i < imagesVoladoras.length; i++){
+    const divContainer = imagesVoladoras[i].parentElement
+    divContainer.style.backgroundColor = "rgba(255,255,0)";
+}
